e94636ef1142f4a697bcd854f3f6f5222d981328
NeverBehave <gayhub@never.pet>
2021-02-01 13:19:28 -0500

    javadoc version and author

--- ConwayGameOfLife/scripts/compile.sh
+++ ConwayGameOfLife/scripts/compile.sh
@@ -3,4 +3,4 @@ echo "[MSG] Compiling..."
 javac -cp './deps/*' -sourcepath src src/com/luox6/conway/Main.java
 echo "[MSG] Compilaton success. Generating Javadoc..."
 cd ./src
-javadoc -encoding UTF-8 com.luox6.conway -d ../docs
+javadoc -author -version -encoding UTF-8 com.luox6.conway -d ../docs
